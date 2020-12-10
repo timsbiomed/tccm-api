5752f2846fe45f56a21f9d42ed623237e17967aa
jiaola <djiao@jhu.edu>
2020-12-10 14:44:43 -0500

    static context files

--- tccm_api/app.py
+++ tccm_api/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from tccm_api.routers import concepts
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title='TCCM API')
 app.include_router(concepts.router)
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/')
 def root():
